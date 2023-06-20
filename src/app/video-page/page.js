e6d3f4c8268da57be03177dbb0747bf6e38bed79
Manikya Sharma <92919520+Manikya-Sharma@users.noreply.github.com>
2023-06-20 16:07:48 +0530

    improved responsiveness

--- src/app/video-page/page.js
+++ src/app/video-page/page.js
@@ -1,86 +1,78 @@
 "use client";
-import { useState } from "react";
-import {
-  Button,
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-} from "@material-tailwind/react";
+import { useState, useEffect } from "react";
+import { Button } from "@material-tailwind/react";
 import ProgressBar from "@ramonak/react-progress-bar";
 
-const startVideo = (progressFunction) => {
+const startVideo = (progressFunction, videoState) => {
   progressFunction(20);
   const videoElement = document.querySelector("#webcam-video");
-  progressFunction(40);
   if (navigator.mediaDevices.getUserMedia) {
+    progressFunction(40);
     navigator.mediaDevices
       .getUserMedia({ video: true })
       .then((stream) => {
         videoElement.srcObject = stream;
+        progressFunction(80);
+        videoElement.play();
         progressFunction(100);
+        videoState(true);
       })
       .catch((error) => {
         console.error(`Could not access video: ${error}`);
+        videoState(false);
       });
   }
-  videoElement.play();
 };
 
-const stopVideo = (progressFunction) => {
-  progressFunction(0);
+const stopVideo = (progressFunction, videoState) => {
   const videoElement = document.querySelector("#webcam-video");
   if (videoElement.srcObject) {
-    progressFunction(40);
     videoElement.srcObject.getTracks().forEach((track) => track.stop());
     videoElement.srcObject = null;
-    progressFunction(100);
+    videoState(false);
+    progressFunction(0);
   }
 };
 
 export default function VideoPage() {
   const [progress, setProgress] = useState(0);
+  const [play, setPlay] = useState(true);
+  useEffect(() => {
+    startVideo(setProgress, setPlay);
+  }, []);
   return (
-    <main>
-      <ProgressBar
-        completed={progress}
-        id="progress-bar"
-        bgColor="blue"
-        height={"5px"}
-        isLabelVisible={false}
-        baseBgColor={"#ffffff"}
-      />
-      <h1 className="text-3xl font-bold tracking-wide w-fit mx-auto my-3">
-        Hand Tracking
-      </h1>
-      <Card className="mb-5">
-        <CardHeader></CardHeader>
-        <CardBody>
+    <>
+      <main>
+        <ProgressBar
+          completed={progress}
+          id="progress-bar"
+          bgColor={"#1199ff"}
+          height={"5px"}
+          isLabelVisible={false}
+          baseBgColor={"#ffffff"}
+          transitionDuration="400ms"
+        />
+        <h1 className="text-4xl font-bold tracking-wide w-fit mx-auto my-3">
+          Hand Tracking
+        </h1>
+        <div className="my-5">
           <video
             id="webcam-video"
-            className="w-[50rem] h-[30rem] bg-gray-400 mx-auto rounded-md [transform:rotateY(180deg)]"
+            className="mx-auto rounded-md [transform:rotateY(180deg)]"
           ></video>
-        </CardBody>
-        <CardFooter></CardFooter>
-      </Card>
-      <div className="flex space-x-4 w-full mx-auto justify-center">
-        <Button
-          onClick={() => {
-            startVideo(setProgress);
-          }}
-          color="green"
-        >
-          Start
-        </Button>
-        <Button
-          onClick={() => {
-            stopVideo(setProgress);
-          }}
-          color="red"
-        >
-          Stop
-        </Button>
-      </div>
-    </main>
+        </div>
+      </main>
+      <Button
+        onClick={() => {
+          play
+            ? stopVideo(setProgress, setPlay)
+            : startVideo(setProgress, setPlay);
+        }}
+        color={play ? "red" : "green"}
+        className="!absolute !bottom-5 !left-[50%] !translate-x-[-50%]"
+      >
+        {play ? "Stop Video" : "Start Video"}
+      </Button>
+    </>
   );
 }
