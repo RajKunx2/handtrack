10891e3985acc7526f85c1b18886030b0590bcf6
Manikya Sharma <92919520+Manikya-Sharma@users.noreply.github.com>
2023-06-20 10:49:59 +0530

    added progrss bar

--- src/app/video-page/page.js
+++ src/app/video-page/page.js
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from "react";
 import {
   Button,
   Card,
@@ -6,14 +7,18 @@ import {
   CardBody,
   CardFooter,
 } from "@material-tailwind/react";
+import ProgressBar from "@ramonak/react-progress-bar";
 
-const startVideo = () => {
+const startVideo = (progressFunction) => {
+  progressFunction(20);
   const videoElement = document.querySelector("#webcam-video");
+  progressFunction(40);
   if (navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices
       .getUserMedia({ video: true })
       .then((stream) => {
         videoElement.srcObject = stream;
+        progressFunction(100);
       })
       .catch((error) => {
         console.error(`Could not access video: ${error}`);
@@ -22,17 +27,29 @@ const startVideo = () => {
   videoElement.play();
 };
 
-const stopVideo = () => {
+const stopVideo = (progressFunction) => {
+  progressFunction(0);
   const videoElement = document.querySelector("#webcam-video");
   if (videoElement.srcObject) {
+    progressFunction(40);
     videoElement.srcObject.getTracks().forEach((track) => track.stop());
     videoElement.srcObject = null;
+    progressFunction(100);
   }
 };
 
 export default function VideoPage() {
+  const [progress, setProgress] = useState(0);
   return (
     <main>
+      <ProgressBar
+        completed={progress}
+        id="progress-bar"
+        bgColor="blue"
+        height={"5px"}
+        isLabelVisible={false}
+        baseBgColor={"#ffffff"}
+      />
       <h1 className="text-3xl font-bold tracking-wide w-fit mx-auto my-3">
         Hand Tracking
       </h1>
@@ -47,10 +64,20 @@ export default function VideoPage() {
         <CardFooter></CardFooter>
       </Card>
       <div className="flex space-x-4 w-full mx-auto justify-center">
-        <Button onClick={startVideo} color="green">
+        <Button
+          onClick={() => {
+            startVideo(setProgress);
+          }}
+          color="green"
+        >
           Start
         </Button>
-        <Button onClick={stopVideo} color="red">
+        <Button
+          onClick={() => {
+            stopVideo(setProgress);
+          }}
+          color="red"
+        >
           Stop
         </Button>
       </div>
