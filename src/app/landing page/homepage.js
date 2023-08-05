3388986eab55f67653790bd69e2edc59b6f16b17
Yash-Choudhary6 <123976594+Yash-Choudhary6@users.noreply.github.com>
2023-08-05 03:29:12 -0700

    landing page initial

---
+++ src/app/landing page/homepage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { ReactDOM } from "react";
+
+
+
+
+
+type Props = {};
+
+const Discover = (props: Props) => {
+  return (
+    
+   
+  );
+};
+
+export default Discover;
